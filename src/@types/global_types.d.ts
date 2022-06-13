27e3a14434870d419b88abee39daf034bec826de
Mateus Louback <mateuslouback@mbp-de-mateus.lan>
2022-06-13 09:30:35 -0300

    fix: restructuring types

---
+++ src/@types/global_types.d.ts
@@ -0,0 +1,10 @@
+declare module '@env';
+declare module '*.png';
+declare module 'lodash';
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
