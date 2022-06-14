8638c31129bd1cd7f652a7670fad990439776ee1
Mateus Louback <mateuslouback@mbp-de-mateus.lan>
2022-06-14 15:26:31 -0300

    feat: add theme aplication

---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  FONT_DARK_COLOR: '#7E7E7E',
+  FONT_CLEAR_COLOR: '#FFFFFF',
+  BG_WEATHER: ['#F8B087', '#93E2C7', '#A1D9E0', '#FDB7C2', '#824CF9'],
+  FONT_SEMI_BOLD_WEIGHT: 'Poppins-SemiBold',
+  FONT_LIGHT_WEIGHT: 'Poppins-Light',
+  FONT_REGULAR_WEIGHT: 'Poppins-Regular',
+};
