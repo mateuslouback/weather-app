eb54efa8773b18243bfc3c0858f324f63690df37
Mateus Louback <mateuslouback@mbp-de-mateus.lan>
2022-06-14 15:35:35 -0300

    feat: adjust style and components to page

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,29 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Alert } from 'react-native';
+import { ThemeContext } from 'styled-components';
 import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location';
-import api from '../../services/api';
-import { API_TOKEN } from '@env';
+import { API_TOKEN } from 'react-native-dotenv';
 import { debounce } from 'lodash';
+import api from '../../api/api';
+import { SceneWrapper } from '../../components/SceneWrapper';
+import { CustomModal } from '../../components/CustomModal';
 import {
   Container,
-  SafeArea,
-  Header,
-  Input,
-  Refresh,
   Icon,
-  RefreshTouchable,
   City,
   Graus,
   Description,
   ScrollCards,
-  ScrollPage,
   Loading,
 } from './styles';
 import { DetailWeather } from '../../components/DetailWeather';
 import { ErrorLocation } from '../../components/ErrorLocation';
 import imageIcon from '../../../assets/images/icons/icons.json';
-import Background from './bg.json';
 
 interface WeatherData {
   main: {
@@ -49,56 +45,112 @@ export const Main = (): JSX.Element => {
   const [hiddenLoad, setHiddenLoad] = useState(false);
   const [weatherData, setWeatherData] = useState<WeatherData>();
   const [icon, setIcon] = useState();
+  const [modalShow, setModalShow] = useState(false);
   const [bgColor, setBgColor] = useState('');
+  const themeContext = useContext(ThemeContext);
 
-  const loadWeather = (location: any) => {
+  function defineBackground(iconWeather: string) {
+    switch (iconWeather) {
+      case '01d':
+      case '50d':
+        setBgColor(themeContext.BG_WEATHER[0]);
+        break;
+      case '01n':
+      case '50n':
+        setBgColor(themeContext.BG_WEATHER[4]);
+        break;
+      case '02d':
+      case '02n':
+      case '03d':
+      case '03n':
+      case '04d':
+      case '04n':
+        setBgColor(themeContext.BG_WEATHER[1]);
+        break;
+      case '09d':
+      case '09n':
+      case '13d':
+      case '13n':
+        setBgColor(themeContext.BG_WEATHER[2]);
+        break;
+      case '10d':
+      case '10n':
+      case '11d':
+      case '11n':
+        setBgColor(themeContext.BG_WEATHER[3]);
+        break;
+      default:
+        setBgColor(themeContext.BG_WEATHER[0]);
+    }
+  }
+
+  const loadWeather = async (locations: Array<number>) => {
     setHiddenLoad(false);
-    api
-      .get(
-        `?lat=${location[0]}&lon=${location[1]}&appid=${API_TOKEN}&lang=pt_br`,
-      )
-      .then(response => {
-        setWeatherData(response?.data);
-        const nameIcon = response?.data.weather[0].icon;
-        setIcon(imageIcon[nameIcon]);
-        defineBackground(nameIcon);
-        setHiddenLoad(true);
-      })
-      .catch(() => {
-        Alert.alert(
-          'Erro ao carregar a previsão do tempo.',
-          'Tente novamente mais tarde.',
-          [{ text: 'OK' }],
-          {
-            cancelable: false,
-          },
-        );
-        setHiddenLoad(true);
-      });
+    try {
+      const response = await api.get(
+        `?lat=${locations[0]}&lon=${locations[1]}&appid=${API_TOKEN}&lang=pt_br`,
+      );
+      setWeatherData(response?.data);
+      const nameIcon = response?.data.weather[0].icon;
+      setIcon(imageIcon[nameIcon]);
+      defineBackground(nameIcon);
+      setHiddenLoad(true);
+    } catch (error) {
+      Alert.alert(
+        'Erro ao carregar a previsão do tempo.',
+        'Tente novamente mais tarde.',
+        [{ text: 'OK' }],
+        {
+          cancelable: false,
+        },
+      );
+      setHiddenLoad(true);
+    }
   };
 
-  const loadWeatherCity = (city: string) => {
+  const loadWeatherCity = async (city: string) => {
     setHiddenLoad(false);
-    api
-      .get(`?q=${city}&appid=${API_TOKEN}&lang=pt_br`)
-      .then(response => {
-        setWeatherData(response?.data);
-        const nameIcon = response?.data.weather[0].icon;
-        setIcon(imageIcon[nameIcon]);
-        defineBackground(nameIcon);
+    try {
+      const response = await api.get(
+        `?q=${city}&appid=${API_TOKEN}&lang=pt_br`,
+      );
+      setWeatherData(response?.data);
+      const nameIcon = response?.data.weather[0].icon;
+      setIcon(imageIcon[nameIcon]);
+      defineBackground(nameIcon);
+      setHiddenLoad(true);
+    } catch (error) {
+      setModalShow(!modalShow);
+      setHiddenLoad(true);
+    }
+  };
+
+  const getLocationUser = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status === 'granted') {
+      const isDeviceLocationEnabled = await Location.hasServicesEnabledAsync();
+
+      if (isDeviceLocationEnabled) {
+        try {
+          const dataLocation = await Location.getCurrentPositionAsync({});
+          const { latitude, longitude } = dataLocation.coords;
+          setLocation([latitude, longitude]);
+          loadWeather([latitude, longitude]);
+        } catch (error) {
+          Alert.alert(
+            'Erro',
+            'O provedor de localização não está disponível. Certifique-se de que os serviços de localização estão ativados',
+          );
+        }
+      } else {
+        setErrorLocation(true);
         setHiddenLoad(true);
-      })
-      .catch(() => {
-        Alert.alert(
-          'Cidade não encontrada!',
-          'Confira o nome da cidade e tente novamente.',
-          [{ text: 'OK' }],
-          {
-            cancelable: false,
-          },
-        );
-        setHiddenLoad(true);
-      });
+      }
+    } else {
+      setErrorLocation(true);
+      setHiddenLoad(true);
+    }
   };
 
   const handleCity = debounce((text: string) => {
@@ -108,71 +160,18 @@ export const Main = (): JSX.Element => {
   }, 1500);
 
   useEffect(() => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-
-      if (status === 'granted') {
-        let isDeviceLocationEnabled = await Location.hasServicesEnabledAsync();
-
-        if (isDeviceLocationEnabled) {
-          try {
-            const dataLocation = await Location.getCurrentPositionAsync({});
-            const { latitude, longitude } = dataLocation.coords;
-            setLocation([latitude, longitude]);
-            loadWeather([latitude, longitude]);
-          } catch (error) {
-            Alert.alert(
-              'Erro',
-              'O provedor de localização não está disponível. Certifique-se de que os serviços de localização estão ativados',
-            );
-          }
-        } else {
-          setErrorLocation(true);
-          setHiddenLoad(true);
-        }
-      } else {
-        setErrorLocation(true);
-        setHiddenLoad(true);
-      }
-    })();
+    getLocationUser();
   }, []);
-
-  function defineBackground(icon: string) {
-    switch (icon) {
-      case '01d':
-      case '50d':
-        setBgColor(Background.color_one);
-        break;
-      case '01n':
-      case '50n':
-        setBgColor(Background.color_five);
-        break;
-      case '02d':
-      case '02n':
-      case '03d':
-      case '03n':
-      case '04d':
-      case '04n':
-        setBgColor(Background.color_two);
-        break;
-      case '09d':
-      case '09n':
-      case '13d':
-      case '13n':
-        setBgColor(Background.color_tree);
-        break;
-      case '10d':
-      case '10n':
-      case '11d':
-      case '11n':
-        setBgColor(Background.color_four);
-        break;
-    }
-  }
 
   return (
     <>
       <StatusBar style="light" />
+      <CustomModal
+        onpress={() => setModalShow(!modalShow)}
+        modalShow={modalShow}
+        title="Cidade não encontrada"
+        subTitle="Confira o nome da cidade e tente novamente."
+      />
       {errorLocation && (
         <ErrorLocation
           title="Sem permissão para acessar sua Localização."
@@ -186,61 +185,46 @@ export const Main = (): JSX.Element => {
           testID="background-color"
           backgroundColor={bgColor || '#fff'}
         >
-          <SafeArea>
-            <Header>
-              <Input
-                testID="input-city"
-                allowFontScaling={false}
-                placeholder="Digite uma cidade"
-                placeholderTextColor="#FFF"
-                keyboardAppearance="dark"
-                onChangeText={text => handleCity(text)}
-                autoCompleteType="off"
-                autoCorrect={false}
-              />
-              <RefreshTouchable onPress={() => loadWeather(location)}>
-                <Refresh
-                  source={require('../../../assets/images/icons/refresh.png')}
+          <SceneWrapper
+            onPress={() => loadWeather(location)}
+            onChangeText={(text: string) => handleCity(text)}
+          >
+            <>
+              <Icon source={{ uri: icon }} />
+              <City>{weatherData?.name}</City>
+              <Graus>
+                {`${(weatherData?.main.temp - 273.15).toFixed(0)}º`}
+              </Graus>
+              <Description>{weatherData?.weather[0].description}</Description>
+              <ScrollCards horizontal showsHorizontalScrollIndicator={false}>
+                <DetailWeather
+                  title="Máxima"
+                  desc={`${(weatherData?.main.temp_max - 273.15).toFixed(0)}º`}
                 />
-              </RefreshTouchable>
-            </Header>
-          </SafeArea>
-          <ScrollPage bounces={false} showsHorizontalScrollIndicator={false}>
-            <Icon source={{ uri: icon }} />
-            <City>{weatherData?.name}</City>
-            <Graus>{(weatherData?.main.temp - 273.15).toFixed(0)}º</Graus>
-            <Description>{weatherData?.weather[0].description}</Description>
-            <ScrollCards
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-            >
-              <DetailWeather
-                title="Máxima"
-                desc={`${(weatherData?.main.temp_max - 273.15).toFixed(0)}º`}
-              />
-              <DetailWeather
-                title="Mínima"
-                desc={`${(weatherData?.main.temp_min - 273.15).toFixed(0)}º`}
-              />
-              <DetailWeather
-                title="Humidade"
-                desc={weatherData?.main?.humidity + '%'}
-              />
-              <DetailWeather
-                title="Pressão"
-                desc={weatherData?.main?.pressure}
-              />
-              <DetailWeather
-                title="Ventos km/h"
-                desc={weatherData?.wind?.speed}
-              />
-              <DetailWeather
-                title="Nuvens"
-                desc={weatherData?.clouds?.all + '%'}
-              />
-              <View style={{ width: 25 }} />
-            </ScrollCards>
-          </ScrollPage>
+                <DetailWeather
+                  title="Mínima"
+                  desc={`${(weatherData?.main.temp_min - 273.15).toFixed(0)}º`}
+                />
+                <DetailWeather
+                  title="Humidade"
+                  desc={`${weatherData?.main?.humidity}%`}
+                />
+                <DetailWeather
+                  title="Pressão"
+                  desc={weatherData?.main?.pressure}
+                />
+                <DetailWeather
+                  title="Ventos km/h"
+                  desc={weatherData?.wind?.speed}
+                />
+                <DetailWeather
+                  title="Nuvens"
+                  desc={`${weatherData?.clouds?.all}%`}
+                />
+                <View style={{ width: 25 }} />
+              </ScrollCards>
+            </>
+          </SceneWrapper>
         </Container>
       )}
     </>
--- src/screens/Main/styles.ts
+++ src/screens/Main/styles.ts
@@ -1,106 +1,100 @@
-import { Dimensions, Platform } from "react-native";
 import styled from 'styled-components/native';
+import { CustomText } from '../../components/CustomText';
+import { normalize } from '../../helpers';
 
 export const Container = styled.View`
   flex: 1;
   flex-direction: column;
   align-items: center;
-  background-color: ${(props: {backgroundColor: string}) => props.backgroundColor};
-`;
-
-export const SafeArea = styled.SafeAreaView`
-  width: 100%;
-`;
-
-export const Header = styled.View`
-  flex-direction: row;
-  align-items: center;
-  margin-vertical: 20px;
-  margin-top: ${Platform.OS === 'android' ? '40px' : '0px'};
-`;
-
-export const Input = styled.TextInput`
-  width: ${(Dimensions.get('window').width - 100) + 'px'};
-  height: 45px;
-  border-width: 1px;
-  border-color: #FFF;
-  background-color: rgba(255, 255, 255, .2);
-  border-radius: 22.5px;
-  padding-horizontal: 17px;
-  font-size: 14px;
-  margin-horizontal: 20px;
-  color: #FFF;
-  font-family: 'Poppins-Light';
-`;
-
-export const RefreshTouchable = styled.TouchableOpacity`
-  height: 40px;
-  width: 40px;
-  margin-right: 20px;
-`;
-
-export const Refresh = styled.Image`
-  height: 40px;
-  width: 40px;
-  margin-right: 20px;
-  resize-mode: contain;
+  background-color: ${(props: { backgroundColor: string }) =>
+    props.backgroundColor};
 `;
 
 export const Icon = styled.Image`
-  width: 220px;
-  height: 220px;
+  width: ${`${normalize(220)}px`};
+  height: ${`${normalize(220)}px`};
   align-self: center;
-  margin-vertical: 20px;
+  margin-vertical: ${`${normalize(20)}px`};
   resize-mode: contain;
 `;
 
-export const City = styled.Text`
+export const City = styled(CustomText)`
   align-self: center;
-  margin-top: 30px;
-  font-size: 30px;
+  margin-top: ${`${normalize(30)}px`};
+  font-size: ${`${normalize(30)}px`};
   text-align: center;
-  color: #FFFFFF;
-  font-family: 'Poppins-Light';
 `;
 
-export const Graus = styled.Text`
+export const Graus = styled(CustomText)`
   align-self: center;
-  font-size: 90px;
+  font-size: ${`${normalize(90)}px`};
   text-align: center;
-  color: #FFFFFF;
-  font-family: 'Poppins-Light';
 `;
 
-export const Description = styled.Text`
+export const Description = styled(CustomText)`
   align-self: center;
-  margin-bottom: 20px;
-  font-size: 16px;
+  margin-bottom: ${`${normalize(20)}px`};
+  font-size: ${`${normalize(16)}px`};
   text-align: center;
-  color: #FFF;
-  font-family: 'Poppins-Light';
   text-transform: capitalize;
 `;
 
 export const ScrollCards = styled.ScrollView`
   width: 100%;
-  margin-top: 40px;
-  margin-bottom: 30px;
-  padding-horizontal: 20px;
+  margin-top: ${`${normalize(40)}px`};
+  margin-bottom: ${`${normalize(40)}px`};
+  padding-horizontal: ${`${normalize(20)}px`};
   align-self: flex-end;
-`;
-
-export const ScrollPage = styled.ScrollView`
-    flex: 1;
-    width: ${Dimensions.get('window').width + 'px'};
-    flex-direction: column;
 `;
 
 export const Loading = styled.ActivityIndicator`
   position: absolute;
   height: 100%;
   width: 100%;
-  background-color: rgba(255,255,255,.5),
+  background-color: rgba(255, 255, 255, 0.5);
   justify-content: center;
   align-items: center;
   z-index: 1000;
+`;
+
+export const ContentModalError = styled.View`
+  background-color: #fff;
+  width: 100%;
+  border-radius: ${`${normalize(8)}px`};
+  height: ${`${normalize(360)}px`};
+  padding: ${`${normalize(20)}px`};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalIcon = styled.Image`
+  width: ${`${normalize(150)}px`};
+  height: ${`${normalize(125)}px`};
+`;
+
+export const TitleModalError = styled(CustomText)`
+  font-size: ${`${normalize(20)}px`};
+  margin-bottom: ${`${normalize(15)}px`};
+  color: ${props => props.theme.FONT_DARK_COLOR};
+`;
+
+export const SubTitleModalError = styled(CustomText)`
+  font-size: ${`${normalize(14)}px`};
+  margin-bottom: ${`${normalize(15)}px`};
+  color: ${props => props.theme.FONT_DARK_COLOR};
+  text-align: center;
+`;
+
+export const ButtonModalError = styled.TouchableOpacity`
+  border: solid ${`${normalize(1)}px`} ${props => props.theme.FONT_DARK_COLOR};
+  border-radius: ${`${normalize(5)}px`};
+  padding: ${`${normalize(10)}px`};
+  width: ${`${normalize(100)}px`};
+  margin-top: ${`${normalize(15)}px`};
+  align-items: center;
+`;
+
+export const TextButtonModalError = styled(CustomText)`
+  font-size: ${`${normalize(12)}px`};
+  color: ${props => props.theme.FONT_DARK_COLOR};
 `;
